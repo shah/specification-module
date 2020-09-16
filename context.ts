f1e88930e439c198474803992415c55ba55b1fc7
Shahid N. Shah <shahid@shah.org>
2020-09-15 23:07:37 -0400

    Resolve Deno 1.4 lint errors

--- context.ts
+++ context.ts
@@ -1,17 +1,15 @@
-import {
+import type {
   Context,
-  ctxFactory,
-  ExecutionEnvironments,
-} from "https://denopkg.com/shah/context-manager@v1.0.5/mod.ts";
-import { Specification } from "./specification.ts";
+} from "https://denopkg.com/shah/context-manager@v1.0.6/mod.ts";
+import type { Specification } from "./specification.ts";
 
 export const DEFAULT_REGISTRY_KEY_MODULE = "igs.spec.lang";
 
 export interface SpecificationContext extends Context {
   readonly isSpecificationContext: true;
-  readonly spec: Specification<any>;
+  readonly spec: Specification<unknown>;
 }
 
-export function isSpecificationContext(o: object): o is SpecificationContext {
-  return "isSpecificationContext" in o;
+export function isSpecificationContext(o: unknown): o is SpecificationContext {
+  return o && typeof o === "object" && "isSpecificationContext" in o;
 }
